08aea55118706900d70108025a494121494c31d7
Nje-ri <138889576+Nje-ri@users.noreply.github.com>
2023-07-26 11:58:02 +0300

    commit initial changes

---
+++ index.js
@@ -0,0 +1,30 @@
+let homePoints = document.getElementById("home-points")
+
+
+function add1(){
+    homePoints.textContent = Number(homePoints.textContent)+1
+}
+
+function add2(){
+    homePoints.textContent = Number(homePoints.textContent)+2
+}
+
+function add3(){
+    homePoints.textContent = Number(homePoints.textContent)+3
+}
+
+
+
+let guestPoints = document.getElementById("guest-points")
+
+function add1g(){
+    guestPoints.textContent = Number(guestPoints.textContent)+1
+}
+
+function add2g(){
+    guestPoints.textContent = Number(guestPoints.textContent)+2
+}
+
+function add3g(){
+    guestPoints.textContent = Number(guestPoints.textContent)+3
+}
